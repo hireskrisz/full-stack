65ed46f42657ae403e3a2b8d2f3598191a090e43
hires.krisztian <hires.krisztian@virgo.hu>
2020-11-06 19:07:42 +0100

    adding deploy wip 3

--- frontend/server.ts
+++ frontend/server.ts
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/frontend'));
+app.use(express.static('./dist/frontend'));
 
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/frontend/'}
+);
 });
 
 // Start the app by listening on the default Heroku port
