8cbc367d72ab94fcdb4fddf50f825cf166aa5ecd
hires.krisztian <hires.krisz@gmail.com>
2020-11-08 22:40:13 +0100

    basic app again

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,21 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {PublicComponent} from "./public/public.component";
-import {SecureComponent} from "./secure/secure.component";
-import {HomeComponent} from "./public/home/home.component";
-import {LoginComponent} from "./public/login/login.component";
 
-const routes: Routes = [
-  {
-    path: '',
-    component: PublicComponent,
-    children: [
-      { path: '', component: HomeComponent },
-      { path: 'login', component: LoginComponent }
-    ]
-  },
-  {path: 'secure', component: SecureComponent}
-];
+
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,22 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SecureComponent } from './secure/secure.component';
-import {PublicModule} from "./public/public.module";
 import {RouterModule} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
-    SecureComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PublicModule,
     RouterModule,
     HttpClientModule
   ],
